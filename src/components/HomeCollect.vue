1452111d789ec9c70f40aa2ac5a15707eed67bd8
Grant Brivael <granttiwa0@gmail.com>
2022-04-11 09:38:09 +0100

    init project

---
+++ src/components/HomeCollect.vue
@@ -0,0 +1,17 @@
+<template>
+    <h3> {{info}} </h3>
+</template>
+
+<script>
+export default {
+    name:'HomeCollect',
+    props:{
+        info: String
+    }
+    
+}
+</script>
+
+<style scoped>
+
+</style>
